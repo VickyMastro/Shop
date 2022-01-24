0bf5d07207b23389488c04d69a0bc85cf9058e3b
VickyMastro <vicmastroianni1702@gmail.com>
2022-01-24 17:14:20 -0300

    Agrego logos e iconos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import '../styles/Header.scss'
 const Header = () => {
   return (
     <nav>
-      <img src="./icons/icon_menu.svg" alt="menu" className="menu" />
+      <img src="../assets/icons/icon_menu.svg" alt="menu" className="menu" />
 
       <div className="navbar-left">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+        <img src="../assets/logos/logo_yard_sale.svg" alt="logo" className="logo" />
 
         <ul>
           <li>
@@ -35,7 +35,7 @@ const Header = () => {
         <ul>
           <li className="navbar-email">platzi@example.com</li>
           <li className="navbar-shopping-cart">
-            <img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
+            <img src="../assets/icons/icon_shopping_cart.svg" alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
